41e46c7d601999fb64d97e4eff0e4fadd9489359
NestorArroyoSerrano <122863244+NestorArroyoSerrano@users.noreply.github.com>
2024-03-04 10:01:09 +0100

    first commit

---
+++ src/main/java/com/nestor/curso/springboot/error/springbooterror/controllers/HandlerExceptionController.java
@@ -0,0 +1,25 @@
+package com.nestor.curso.springboot.error.springbooterror.controllers;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.nestor.curso.springboot.error.springbooterror.models.Error;
+
+@RestControllerAdvice
+public class HandlerExceptionController {
+
+    @ExceptionHandler({ArithmeticException.class})
+    public ResponseEntity<Error> divisionByZero(Exception ex) {
+        Error error = new Error();
+        error.setDate(new Date());
+        error.setError("Error división por cero!");
+        error.setMessage(null);
+        error.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+       // return ResponseEntity.internalServerError().body(error);
+       return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR.value()).body(error);
+    }
+}
